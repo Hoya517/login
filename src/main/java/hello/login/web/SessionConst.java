d5a8d86fd58f58276acde7a90001d3e599b26a35
Kim Juho <sv05159@naver.com>
2021-09-10 14:48:33 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1
    - use HttpSession

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,13 @@
+package hello.login.web;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
+
+//public interface SessionConst {
+//    String LOGIN_MEMBER = "loginMember";
+//}
+
+//public abstract class SessionConst {
+//    public static final String LOGIN_MEMBER = "loginMember";
+//}
